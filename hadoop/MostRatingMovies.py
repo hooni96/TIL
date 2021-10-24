ac72617755877d7f2dec91f11112e89652583de9
JihunMin <minzihun@gmail.com>
2021-10-25 08:29:09 +0900

    [Hadoop] Update spark HW1

--- hadoop/MostRatingMovies.py
+++ hadoop/MostRatingMovies.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     movies = loadMovies()
     path = "hdfs:///user/maria_dev/ml-latest-small/ratings.csv"
 
-    # create spart context
+    # create spark context
     conf = SparkConf().setAppName("MostRatingMovies")
     sc = SparkContext(conf = conf) # sc밑에 spark operater 있음!
 
@@ -40,19 +40,18 @@ if __name__ == "__main__":
     # RDD가 계속 만들어 지고 있는 중
     ratings = lines.map(parseInput)
 
-    # reduce to (movieId, (sumOfRating, countRating))
+    # reduce to (movieId, (sumOfRating, countRating)) 즉 rating 합과 개수를 뱉어냄.
     # 다음 결과에 메모리 되서 들어가는.. S = S + a 라고 생각하면 됨
-    # reduceByKey action!
     sumAndCounts = ratings.reduceByKey(lambda m1, m2: (m1[0] +m2[0], m1[1] +m2[1]))
 
-    # sumAndCount --> (movieId, averageRating) 평점
-    avgRatings = sumAndCounts.mapValues(lambda v: v[0] / v[1])
+    # sumAndCount --> (movieId, (averageRating, countRating)) 평점 구하기!
+    avgRatings = sumAndCounts.mapValues(lambda v: (v[0] / v[1], v[1]))
     
     # filter를 걸어서 평균이 2.0 안되는 녀석들만 걸러준다.
-    filterRatings = avgRatings.filter(lambda x: x[1] < 2.0)
+    filterRatings = avgRatings.filter(lambda x: x[1][0] < 2.0)
     
     # sort
-    sortedMovies = filterRatings.sortBy(lambda x: x[1], ascending=False)
+    sortedMovies = filterRatings.sortBy(lambda x: x[1][1], ascending=False)
 
     # top 10 / take에서 action
     results = sortedMovies.take(30)
@@ -60,4 +59,4 @@ if __name__ == "__main__":
     # reduceByKey랑 take에서 action이 일어난다. 실질적이 계산이 이뤄짐.
     
     for result in results:
-        print(movies[result[0]], result[1])
+        print(movies[result[0]], result[1][0], result[1][1])
